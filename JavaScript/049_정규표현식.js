2c8dbebd4edf013a3a479d4148b65d7bce30fd73
hyejihan <108308915+hyejee0504@users.noreply.github.com>
2022-10-14 23:07:18 +0900

    update

---
+++ JavaScript/049_정규표현식.js
@@ -0,0 +1,7 @@
+("paullab CEO leehojun hello CEO").replace(/CEO/, "CTO");
+("paullab CEO leehojun hello CEO").replace(/CEO/g, "CTO");
+
+("paullab CEO leehojun hello CEO").match(/CEO/g);
+
+
+(/CEO/g).test("paullab CEO leehojun hello CEO");
