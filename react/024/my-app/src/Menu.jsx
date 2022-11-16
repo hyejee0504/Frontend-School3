b564273dbcd06679f1e08f6d48da1950d31131e5
hyejihan <108308915+hyejee0504@users.noreply.github.com>
2022-11-16 15:26:06 +0900

    update

---
+++ react/024/my-app/src/Menu.jsx
@@ -0,0 +1,19 @@
+import One from './components/One'
+import Two from './components/Two'
+import Three from './components/Three'
+import Four from './components/Four'
+import Five from './components/Five'
+import React from 'react';
+
+const Menu = () => {
+    return (
+        <nav>
+            <ul>
+                
+            </ul>
+        </nav>
+    );
+}
+
+
+export default Menu;
